21a459f557c46ff7b5cca4c878e5b423a16d2558
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-11-06 22:46:40 +0700

    feat: create drizzle.config.js

---
+++ drizzle.config.js
@@ -0,0 +1,15 @@
+// drizzle.config.ts
+import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
+
+export default defineConfig({
+  schema: './db/schema.js',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  strict: true,
+});
