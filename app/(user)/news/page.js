75f9adf94505db1c9b32581fd404b3818e704854
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-11-07 08:44:39 +0700

    update page.js

--- app/(user)/news/page.js
+++ app/(user)/news/page.js
@@ -1,3 +1,15 @@
 export default async function NewsPage() {
-  return <div>Halo dari Admin News Page</div>;
+  return (
+    <main className="h-screen snap-y snap-mandatory overflow-y-scroll">
+      <section className="flex h-screen w-screen snap-start items-center justify-center text-black">
+        <div className="flex max-w-4xl flex-col items-center gap-8 px-6 text-center">
+          {' '}
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl leading-tight font-bold">News</h1>
+            <p className="text-lg text-black/50">Lorem Ipsoadio asjdnonafp </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
