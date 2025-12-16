8041490e260b1cc88f98bc84b2d4375482e3df88
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-12-16 11:30:05 +0700

    update ProjectCard.js

--- app/(user)/researcher/project/_components/ProjectCard.js
+++ app/(user)/researcher/project/_components/ProjectCard.js
@@ -1,13 +1,17 @@
+import Link from 'next/link';
+
 export default function ProjectCard({ title, date, description }) {
   return (
-    <div className="flex min-h-[200px] w-full flex-col gap-2 rounded-3xl bg-white p-4 outline outline-1 outline-zinc-300 sm:p-6">
-      <div className="text-lg font-medium text-zinc-800 sm:text-xl">
-        {title}
+    <Link href="/researcher/project/detail" className="block">
+      <div className="flex min-h-[200px] w-full cursor-pointer flex-col gap-2 rounded-3xl bg-white p-4 outline outline-1 outline-zinc-300 transition hover:shadow-md sm:p-6">
+        <div className="text-lg font-medium text-zinc-800 sm:text-xl">
+          {title}
+        </div>
+        <div className="text-xs text-neutral-400 sm:text-sm">{date}</div>
+        <div className="line-clamp-4 text-sm text-neutral-500 sm:text-base">
+          {description}
+        </div>
       </div>
-      <div className="text-xs text-neutral-400 sm:text-sm">{date}</div>
-      <div className="line-clamp-4 text-sm text-neutral-500 sm:text-base">
-        {description}
-      </div>
-    </div>
+    </Link>
   );
 }
